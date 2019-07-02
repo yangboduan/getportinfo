e338b352ccacc567c51548f1009431bf40ad07e3
Administrator <Administrator@DESKTOP-4T6I28J>
2019-07-03 00:46:16 +0800

    添加项目文件。

---
+++ GetPortInfo/main.cpp
@@ -0,0 +1,113 @@
+//ref url:https://docs.microsoft.com/zh-cn/windows/win32/api/tcpmib/ns-tcpmib-_mib_tcptable
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <iphlpapi.h>
+#include <stdio.h>
+
+#pragma comment(lib, "iphlpapi.lib")
+#pragma comment(lib, "ws2_32.lib")
+
+#define MALLOC(x) HeapAlloc(GetProcessHeap(), 0, (x))
+#define FREE(x) HeapFree(GetProcessHeap(), 0, (x))
+
+/* Note: could also use malloc() and free() */
+
+int main()
+{
+	// Declare and initialize variables
+	PMIB_TCPTABLE pTcpTable;
+	DWORD dwSize = 0;
+	DWORD dwRetVal = 0;
+
+	char szLocalAddr[128];
+	char szRemoteAddr[128];
+
+	struct in_addr IpAddr;
+
+	int i;
+
+	pTcpTable = (MIB_TCPTABLE*)MALLOC(sizeof(MIB_TCPTABLE));
+	if (pTcpTable == NULL) {
+		printf("Error allocating memory\n");
+		return 1;
+	}
+
+	dwSize = sizeof(MIB_TCPTABLE);
+
+	// Make an initial call to GetTcpTable to
+	// get the necessary size into the dwSize variable
+	if ((dwRetVal = GetTcpTable(pTcpTable, &dwSize, TRUE)) ==
+		ERROR_INSUFFICIENT_BUFFER) {
+		FREE(pTcpTable);
+		pTcpTable = (MIB_TCPTABLE*)MALLOC(dwSize);
+		if (pTcpTable == NULL) {
+			printf("Error allocating memory\n");
+			return 1;
+		}
+	}
+
+	// Make a second call to GetTcpTable to get
+	// the actual data we require
+	if ((dwRetVal = GetTcpTable(pTcpTable, &dwSize, TRUE)) == NO_ERROR) {
+		printf("\tNumber of entries: %d\n", (int)pTcpTable->dwNumEntries);
+		for (i = 0; i < (int)pTcpTable->dwNumEntries; i++) {
+			printf("\n\tTCP[%d] State: %ld - ", i, pTcpTable->table[i].dwState);
+			switch (pTcpTable->table[i].dwState) {
+			case MIB_TCP_STATE_CLOSED:
+				printf("CLOSED\n");
+				break;
+			case MIB_TCP_STATE_LISTEN:
+				printf("LISTEN\n");
+				break;
+			case MIB_TCP_STATE_SYN_SENT:
+				printf("SYN-SENT\n");
+				break;
+			case MIB_TCP_STATE_SYN_RCVD:
+				printf("SYN-RECEIVED\n");
+				break;
+			case MIB_TCP_STATE_ESTAB:
+				printf("ESTABLISHED\n");
+				break;
+			case MIB_TCP_STATE_FIN_WAIT1:
+				printf("FIN-WAIT-1\n");
+				break;
+			case MIB_TCP_STATE_FIN_WAIT2:
+				printf("FIN-WAIT-2 \n");
+				break;
+			case MIB_TCP_STATE_CLOSE_WAIT:
+				printf("CLOSE-WAIT\n");
+				break;
+			case MIB_TCP_STATE_CLOSING:
+				printf("CLOSING\n");
+				break;
+			case MIB_TCP_STATE_LAST_ACK:
+				printf("LAST-ACK\n");
+				break;
+			case MIB_TCP_STATE_TIME_WAIT:
+				printf("TIME-WAIT\n");
+				break;
+			case MIB_TCP_STATE_DELETE_TCB:
+				printf("DELETE-TCB\n");
+				break;
+			default:
+				printf("UNKNOWN dwState value: %d\n", pTcpTable->table[i].dwState);
+				break;
+			}
+		}
+	}
+	else {
+		printf("\tGetTcpTable failed with %d\n", dwRetVal);
+		FREE(pTcpTable);
+		return 1;
+	}
+
+	if (pTcpTable != NULL) {
+		FREE(pTcpTable);
+		pTcpTable = NULL;
+	}
+
+	return 0;
+
+}
